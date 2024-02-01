1c15b5c76e61f41c05a07d94512e7ee6f6304c78
zenonix <purandhar@proton.me>
2024-02-01 16:56:04 +0530

    initial commit

---
+++ 2sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        count = 0
+        while count < len(nums):
+            sub = target - nums[count]
+            if (sub in nums) and (nums.index(sub) != count):
+                result = [count, nums.index(sub)]
+                return result
+                count = len(nums) + 1
+            else:
+                count += 1
+
+
+Solution().twoSum([2, 7, 11, 15], 9)
